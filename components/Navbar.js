251a413040e1562b27d6c38351ee80d90885c7b7
NinadMaladkar <maladkar.ninad@gmail.com>
2023-01-25 12:51:48 +0100

    Add layout, navbar and footer components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Sony Headphones</Link>
+      </p>
+      <button type='button' className='cart-icon'>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>5</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
